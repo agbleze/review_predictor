b0979b826240b69b1269979f4ccdb40df9cdda0a
linus <lagbleze@st.ug.edu.gh>
2025-07-18 01:10:14 +0200

    fix: add default config values

--- app/constant.py
+++ app/constant.py
@@ -1,7 +1,7 @@
 from decouple import config
 
-HOST = config('HOST')
-PORT = config('PORT')
-ENDPOINT = config('ENDPOINT')
+HOST = config('HOST', default='0.0.0.0')
+PORT = config('PORT', default=8000)
+ENDPOINT = config('ENDPOINT', default="predict")
 
 URL = f'{HOST}:{PORT}/{ENDPOINT}'
